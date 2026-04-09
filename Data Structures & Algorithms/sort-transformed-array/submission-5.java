d57fe79c89845031196d473fa4d444249694defe
Sumant <scschaudhary81@gmail.com>
2026-04-09 14:39:48 +0530

    Add: sort-transformed-array - submission-5

---
+++ Data Structures & Algorithms/sort-transformed-array/submission-5.java
@@ -0,0 +1,51 @@
+class Solution {
+    // if upward parabola the terminal values will always give larger value
+    // if downward parabola the terminal values will always give smallest value
+    // use two pointer start and end and choose larger/smaller values according
+    public int quad(int x,int a,int b,int c){
+        return a*x*x+b*x+c;
+    }
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int start = 0;
+        int end = nums.length-1; 
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+        while(start<=end){
+            int startVal = quad(nums[start],a,b,c);
+            int endVal = quad(nums[end],a,b,c);
+            // if upwards parabola (higher x higher y)
+            if(a>=0){
+                if(startVal >= endVal){
+                    list.add(startVal);
+                    start++;
+                }else{
+                    list.add(endVal);
+                    end--;
+                }
+            }
+            // if downward parabola (higher x lower y)
+            if(a<0){
+                if(startVal <= endVal){
+                    list.add(startVal);
+                    start++;
+                }else{
+                    list.add(endVal);
+                    end--;
+                }
+            }
+        }
+
+        return a>=0 
+        ?
+        IntStream
+        .range(0,list.size())
+        .map(i->list.get(list.size()-1-i))
+        .toArray()
+        :
+        list
+        .stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+    }
+}
