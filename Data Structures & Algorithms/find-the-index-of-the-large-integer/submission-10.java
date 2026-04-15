6e751f27522e5b48d0a25b61311720192f9f95e7
Sumant <scschaudhary81@gmail.com>
2026-04-15 16:35:24 +0530

    Add: find-the-index-of-the-large-integer - submission-10

---
+++ Data Structures & Algorithms/find-the-index-of-the-large-integer/submission-10.java
@@ -0,0 +1,48 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     // Compares the sum of arr[l..r] with the sum of arr[x..y] 
+ *     // return 1 if sum(arr[l..r]) > sum(arr[x..y])
+ *     // return 0 if sum(arr[l..r]) == sum(arr[x..y])
+ *     // return -1 if sum(arr[l..r]) < sum(arr[x..y])
+ *     public int compareSub(int l, int r, int x, int y) {}
+ *
+ *     // Returns the length of the array
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+    public int getIndex(ArrayReader reader) {
+        int start = 0;
+        int end = reader.length()-1;
+        int index = -1;
+
+
+        while(start<=end){
+            boolean isEven = (end-start+1) % 2 == 0;
+            int mid = (end+start)/2;
+            
+            if(start==end){ // if start == end
+                index=start;
+                break;
+            }else if(isEven){ // if even partions
+                int compare = reader.compareSub(start,mid,mid+1,end);
+                if(compare == 1) end = mid;
+                if(compare == -1) start = mid + 1;
+            }else{ // if odd partions
+                int compare = reader.compareSub(start,mid-1,mid+1,end);
+                if(compare == 1) end = mid-1;
+                else if(compare == -1) start = mid + 1;
+                else{
+                    index = mid;
+                    break;
+                }
+            }
+        }   
+
+
+        return index;
+    }
+}
