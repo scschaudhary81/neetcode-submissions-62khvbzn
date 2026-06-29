65ceef66c9e16059040261231f641965895b7be9
Sumant <scschaudhary81@gmail.com>
2026-06-29 21:59:45 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-4

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDifference(String s) {
+        int[] memory = new int[26];
+        for(char c : s.toCharArray()){
+            memory[c-'a']++;
+        }
+        int odd_max = Integer.MIN_VALUE;
+        int even_min = Integer.MAX_VALUE;
+
+        for(int  i : memory){
+            if( i == 0 ) continue;
+            else if(i % 2 == 0)even_min = Math.min(even_min,i); 
+            else odd_max = Math.max(odd_max,i);
+        }
+
+        return odd_max == Integer.MIN_VALUE || even_min == Integer.MAX_VALUE ? -1 : odd_max - even_min;
+    }
+}
