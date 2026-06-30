44b75dbd914ac0e00d2f9e6139f6b936bee2bc5f
Sumant <scschaudhary81@gmail.com>
2026-06-30 21:39:12 +0530

    Add: populating-next-right-pointers-in-each-node - submission-2

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-2.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+/*
+Do BFS and keep track of prev and current
+if prev != null prev.next = current
+*/
+
+class Solution {
+    public void addNodeIfNotNull(Node root,Queue<Node> q){
+        if(root != null) q.offer(root);
+    }
+    public Node connect(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        addNodeIfNotNull(root,q);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            Node prev = null;
+            while(size > 0){
+                Node current = q.poll();
+                if(prev != null) prev.next = current;
+                addNodeIfNotNull(current.left,q);
+                addNodeIfNotNull(current.right,q);
+                size--;
+                prev=current;
+            }
+        }
+        return root;
+    }
+}
