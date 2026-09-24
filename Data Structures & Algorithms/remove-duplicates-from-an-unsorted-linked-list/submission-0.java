2752c46f0de2439982ec2ce46aca83d425febdfd
Sumant <scschaudhary81@gmail.com>
2026-09-24 23:56:01 +0530

    Add: remove-duplicates-from-an-unsorted-linked-list - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-an-unsorted-linked-list/submission-0.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode deleteDuplicatesUnsorted(ListNode head) {
+        ListNode start = new ListNode(-1); start.next = head;
+        ListNode prev = start;
+        ListNode current = head;
+        HashMap<Integer,Integer> memory = new HashMap<>();
+
+        ListNode iterate = head;
+        while(iterate != null){
+            memory.put(iterate.val,memory.getOrDefault(iterate.val,0)+1);
+            iterate = iterate.next;
+        }
+
+
+        while(current != null){
+            if(memory.get(current.val) > 1){
+                prev.next = current.next;
+            }else{
+                prev=current;
+            }
+            current = current.next;
+        }   
+
+
+        return start.next;
+    }
+}
